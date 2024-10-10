50696aaf0d49cf4a40ff16c42dcc4cd0c3d55be1
yecibeth rivera <riverayecibeth@gmail.com>
2024-10-10 11:42:00 -0500

    Subo contacto y agenda

--- src/Contacto.java
+++ src/Contacto.java
@@ -1,2 +1,36 @@
 public class Contacto {
+    protected String nombre;
+    protected String apellido;
+    protected double telefono;
+
+    public Contacto(double telefono, String nombre, String apellido) {
+        this.telefono = telefono;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public double getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(double telefono) {
+        this.telefono = telefono;
+    }
 }
+
